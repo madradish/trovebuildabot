5158d87072df38c7c3fd64fc572fa8f8c6f67069
madradish <14355389+madradish@users.noreply.github.com>
2018-05-28 13:58:21 +0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@
 # Twitter keys
 consumer_key = ''
 consumer_secret = ''
-access_token_key = ''
-access_token_secret = ''
+access_token_key = 'Nn5TsupErU8mVDoUk8cEvA'
+access_token_secret = 'fLLgNml4lnuLHysOSx699nk7BRgyin6ERz0N9atk'
 # Trove API key
-api_key = ''
+api_key = '251le1tsa2l0r9n4'
