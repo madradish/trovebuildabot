91cd3dfd084b9c34c8df693a22826d8146717d5e
madradish <14355389+madradish@users.noreply.github.com>
2018-06-01 14:21:10 +0800

    Update credentials.py
    Added credentials

--- credentials.py
+++ credentials.py
@@ -1,8 +1,8 @@
 # Insert your key values below.
 # Twitter keys
-consumer_key = ''
-consumer_secret = ''
-access_token_key = 'Nn5TsupErU8mVDoUk8cEvA'
-access_token_secret = 'fLLgNml4lnuLHysOSx699nk7BRgyin6ERz0N9atk'
+consumer_key = 'Nn5TsupErU8mVDoUk8cEvA'
+consumer_secret = 'fLLgNml4lnuLHysOSx699nk7BRgyin6ERz0N9atk'
+access_token_key = '2395801730-8mW5ZmvS5pszcEVnxaAdyK6ZVX8xCBcqmoNDPMk'
+access_token_secret = 'SQY02gaxn43NBPmIVWB8XGbhKqsv3yNbc7fEia5CZd4jm'
 # Trove API key
 api_key = '251le1tsa2l0r9n4'
